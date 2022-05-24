48abb14d9d6792995a4f4b1759979ac5aaae4ea8
Andrey Potyomkin <and_tem@icloud.com>
2022-05-24 11:21:59 +0300

    feat(cryptopro-cades): Настроено развертывание example на gh-pages

--- examples/cryptopro-cades/webpack.config.js
+++ examples/cryptopro-cades/webpack.config.js
@@ -10,6 +10,7 @@ const { useUrlImages } = require('@webpackon/use-url-images');
 const { useDevServer } = require('@webpackon/use-dev-server');
 const { useTranspileModules } = require('@webpackon/use-transpile-modules');
 const { useCss } = require('@webpackon/use-css');
+const { useOptimization } = require('@webpackon/use-optimization');
 
 module.exports = (_, { mode }) =>
   compose(
@@ -26,7 +27,13 @@ module.exports = (_, { mode }) =>
     useCss({ mode }),
     useFonts(),
     useUrlImages({ mode }),
-    useDevServer({ mode, port: 3000 })
+    useDevServer({ mode, port: 3000 }),
+    useOptimization({
+      mode,
+      splitChunkCacheGroups: [
+        { chunkName: 'react', includePackages: ['react', 'react-dom'] },
+      ],
+    })
   )({
     target: 'web',
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
