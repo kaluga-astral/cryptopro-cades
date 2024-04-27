fbb54b4001951d6758c69e6ef2717a1455aadab1
va4es2 <sukhachev_vs@astralnalog.ru>
2024-04-27 14:35:33 +0300

    bug(cryptopro-cades): Поиск ключа по Skid всегда вызывал поиск по всем носителям с проверкой закрытого ключа, даже если не требуется. Добавил параметры вызова функции поиска сертификата для гибкости.

--- packages/cryptopro-cades/src/api/findCertificateBySkid.ts
+++ packages/cryptopro-cades/src/api/findCertificateBySkid.ts
@@ -1,4 +1,5 @@
 import type { Certificate } from '../Certificate';
+import { STORE_TYPE } from '../constants';
 import { CryptoError } from '../errors';
 
 import { getCertificates } from './../api';
@@ -6,11 +7,15 @@ import { getCertificates } from './../api';
 /**
  * Поиск в хранилищах сертификата.
  * @param {string} subjectKeyId -идентификатор ключа субъекта.
+ * @param {STORE_TYPE} storeType в каком хранилище требуется поискать сертификат (из токена, реестра, все...).
+ * @param {boolean} [checkPrivateKey=false] проводить проверку наличия закрытого ключа.
  * @throws {CryptoError} в случае ошибки.
  * @returns {@Promise<Certificate | undefined>} сертификат.
  */
 export async function findCertificateBySkid(
   subjectKeyId: string,
+  storeType: STORE_TYPE = STORE_TYPE.ALL,
+  checkPrivateKey: boolean = false,
 ): Promise<Certificate | undefined> {
   if (!subjectKeyId) {
     const errorMessage =
@@ -20,10 +25,11 @@ export async function findCertificateBySkid(
   }
 
   try {
-    return (await getCertificates()).find(
+    return (await getCertificates(storeType, false, checkPrivateKey)).find(
       (c) => c.subjectKeyId == subjectKeyId,
     );
   } catch (err) {
     throw CryptoError.createCadesError(err, 'Ошибка получения сертификата.');
   }
 }
+
